212a1b42383a67cc83d080cd1592e40e262ba6b8
CioF <coder.walidhasan@gmail.com>
2025-04-11 12:37:02 +0600

    custom model save() for functional auto word count.

--- app_dj/models.py
+++ app_dj/models.py
@@ -1,25 +1,30 @@
-from django.db import models
+import re
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 ARTICLE_STATUS = (
-            ("draft", "draft"),
-            ("inprogress", "in progress"),
-            ("published", "published"),
-        )
+    ("draft", "draft"),
+    ("inprogress", "in progress"),
+    ("published", "published"),
+)
+
 
 # Create your models here.
 class UserProfile(AbstractUser):
     pass
 
+
 class Articles(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default="")
-    word_count = models.IntegerField()
+    word_count = models.IntegerField(blank=True, default=0)
     twitter_post = models.TextField(blank=True, default="")
-    status = models.CharField(
-        max_length=20,
-        choices=ARTICLE_STATUS,
-        default="draft"
-    )
+    status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default="draft")
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
+        self.word_count = len(re.findall(r"\b\w+\b", text))
+        super().save(*args, **kwargs)
