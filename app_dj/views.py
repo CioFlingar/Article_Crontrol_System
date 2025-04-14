3cb58028d85c9d020f88bcd6613a35b1161fe524
CioF <coder.walidhasan@gmail.com>
2025-04-14 17:30:41 +0600

    secure views to specific users

--- app_dj/views.py
+++ app_dj/views.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
@@ -31,13 +35,18 @@ from app_dj.models import Articles
 #         return render(request, "app_dj/article_create.html", {"form": form})
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "app_dj/home.html"
     model = Articles
     context_object_name = "articles"
 
+    def get_queryset(self) -> QuerySet[Any]:
+        return Articles.objects.filter(creator=self.request.user).order_by(
+            "-created_at"
+        )
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = "app_dj/article_create.html"
     model = Articles
     fields = ["title", "status", "content", "twitter_post"]
@@ -49,16 +58,22 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
 
-class AritcleUpdateView(UpdateView):
+class AritcleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "app_dj/article_update.html"
     model = Articles
     fields = ["title", "status", "content", "twitter_post"]
     success_url = reverse_lazy("home")
     context_object_name = "article"
 
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "app_dj/article_delete.html"
     model = Articles
     success_url = reverse_lazy("home")
     context_object_name = "article"
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
