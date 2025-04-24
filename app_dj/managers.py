8c6d194a8543159b07ff53334decaa24cb275177
CioF <coder.walidhasan@gmail.com>
2025-04-24 11:10:40 +0600

    add remain html file, with adding styles

--- app_dj/managers.py
+++ app_dj/managers.py
@@ -11,7 +11,7 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def Create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
